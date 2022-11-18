b454cd0bafe0ae028afce44f102ee9cd45eb4947
SarahVee <sarahvacio@gmail.com>
2022-11-17 21:45:46 -0500

    started over

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var Class = require('../models/class');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	Class.getClasses(function(err, classes){
-		res.render('index', { classes: classes });
-	},3);
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
